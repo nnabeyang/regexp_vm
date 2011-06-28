bed90f11818cc711f29379bf1b66d8011a028201
noriaki watanabe <nabeyang@gmail.com>
2011-06-28 17:43:39 +0900

    1st commit

---
+++ nfa.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+struct Regexp {
+  int type;
+  int ch;
+  struct Regexp* left;
+  struct Regexp* right;
+};
+enum {
+  Lit = 1,
+  Cat,
+};
+struct Regexp* reg(int type, struct Regexp* left, struct Regexp* right) {
+struct Regexp* re = (struct Regexp*)malloc(sizeof(struct Regexp));
+  re->type = type; re->left = left; re->right = right;
+  return re;
+}
+void reg_to_str(char* str, struct Regexp* re) {
+  switch(re->type) {
+    default:
+      assert(0);
+    case Lit:
+      sprintf(str, "Lit(%c)", re->ch);
+      break;
+    case Cat: {
+      char lbuf[80], rbuf[80];
+      reg_to_str(lbuf, re->left);
+      reg_to_str(rbuf, re->right);
+      sprintf(str, "Cat(%s, %s)", lbuf, rbuf);
+      break;
+      }
+  }
+}
+void test(void);
+int main(void) {
+  test();
+  return 0;
+}
+void test_reg(void) {
+struct Regexp* lit1 = reg(Lit, NULL, NULL);
+lit1->ch = 'a';
+struct Regexp* lit2 = reg(Lit, NULL, NULL);
+lit2->ch = 'b';
+
+char str[80];
+reg_to_str(str, lit1);
+assert(!strcmp("Lit(a)", str));
+
+struct Regexp* cat = reg(Cat, lit1, lit2);
+
+reg_to_str(str, cat);
+assert(!strcmp("Cat(Lit(a), Lit(b))", str));
+
+free(lit1); free(lit2); free(cat);
+}
+
+void test(void) {
+  test_reg();
+}
